9d7b2e77bc70e62f0081d254030f1db2397ca3da
Elena-Abreu <ena3@njit.edu>
2024-11-21 10:19:28 -0500

    Question 3

--- lab.js
+++ lab.js
@@ -6,4 +6,13 @@ characters.forEach(element => { console.log(element)});
 let firstName = "Harry"; 
 let house = "griffindor"
 let greeting = `Welcome, ${firstName} of ${house}!`;
-console.log(greeting);
+console.log(greeting); 
+
+// toUpperCase() and toLowerCase() examples
+let characterName = "Hermione";
+console.log( characterName.toUpperCase());
+
+console.log( characterName.toLowerCase());
+//output:
+//toUpperCase(): HELLO, JAVASCRIPT!
+//toLowerCase(): hello, javascript!
