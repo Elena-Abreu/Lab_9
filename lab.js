f1343a2394a2e0cd70242f6b8923e53cde4852f4
Elena-Abreu <ena3@njit.edu>
2024-11-21 10:48:36 -0500

    Updated Question 8 and did 9

--- lab.js
+++ lab.js
@@ -39,9 +39,24 @@ sentence = 'Draco is a good wizard.';
 let replacedStr = sentence.replace("good", "bad");
 console.log(replacedStr);
 
-let arr = ['Griffindor', 'Hufflepuff', 'Ravenclaw'];
-arr.push('Slytherin');
-console.log("After push(4, 5):", arr);
+let HOUSES = ['Griffindor', 'Hufflepuff', 'Ravenclaw'];
+HOUSES.push('Slytherin');
+console.log("After push(4, 5):", HOUSES);
 
-let lastElement = arr.pop();
-console.log("After pop():", arr);
+let lastElement = HOUSES.pop();
+console.log("After pop():", HOUSES);
+
+
+
+let SPELL = ['Alohomora', 'Lumos', 'Nox'];
+// unshift() example (add elements to the beginning)
+SPELL.unshift('Accio');
+// arr.unshift(100,200,300); you can multiple elements
+console.log(SPELL);
+//output:
+//After unshift(100):[100, 1, 2, 3, 4]
+// shift() example (remove the first element, one at a time)
+let firstElement = SPELL.shift();
+console.log(SPELL);
+
+
