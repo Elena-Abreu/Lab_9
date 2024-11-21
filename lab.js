a6d9bd0e168136d5c41a0bee52a6c6833a0af44d
Elena-Abreu <ena3@njit.edu>
2024-11-21 10:16:28 -0500

    Question 2

--- lab.js
+++ lab.js
@@ -1,4 +1,9 @@
 let characters = ['Harry', 'Hermione', 'Ron', 'Draco', 'Luna']
-characters.forEach(element => { console.log(element)
-    
-});
+characters.forEach(element => { console.log(element)});  
+
+
+
+let firstName = "Harry"; 
+let house = "griffindor"
+let greeting = `Welcome, ${firstName} of ${house}!`;
+console.log(greeting);
